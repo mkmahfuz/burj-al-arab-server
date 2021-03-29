26cff5cc47dd4c8e8d0f03ed563117e58dda7b7c
khalid <mdmahfuzbd@gmail.com>
2021-03-29 19:38:51 +0600

    module-47-4 done

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+//get user/pass from env
+require('dotenv').config();
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req, res) =>{
+    res.send("hello world")
+})
+
+app.listen(port,()=>{
+    console.log("I am listening at port: ",port)
+})
+
+//mongodb connection
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.lroqv.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true ,connectTimeoutMS: 30000,keepAlive: 1});
+client.connect(err => {
+  const collection = client.db("burjAlArab").collection("bookins");
+  // perform actions on the collection object
+  console.log('mongodb connection success');
+
+  app.post('/addBooking',(req, res) => {
+      const newBooking = req.body;
+      collection.insertOne(newBooking)
+      .then(result=>{
+          res.send(result.insertedCount > 0);
+          console.log(result.insertedCount);
+      })
+      //res.send(newBooking);
+      console.log(newBooking);
+  })
+
+  //client.close ta off kore dilam
+  //client.close();
+});
